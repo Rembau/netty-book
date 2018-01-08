ade45528301fa1ed7039e90ed54cf472f35c780b
rembau <504229406@qq.com>
2018-01-08 22:13:04 +0800

    改成maven项目

---
+++ src/test/java/test/Test.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Test {
+    public static void main(String args[]) {
+        System.out.println(Integer.toBinaryString(2));
+        System.out.println(Integer.toBinaryString(~2));
+        System.out.println(~2);
+    }
+}
